f40c36d5419f8e58b3aa8089345cfe8dff73e93f
Harfin <harfinaqbil07@gmail.com>
2024-05-23 12:45:29 +0700

    inisiasi project tes2

---
+++ src/scripts/index.js
@@ -0,0 +1,19 @@
+import "regenerator-runtime";
+import "../styles/style.css";
+import "../styles/responsive.css";
+import "../styles/home-style.css";
+import App from "./views/app";
+
+const app = new App({
+  button: document.querySelector(".tombol"),
+  drawer: document.querySelector(".menu"),
+  content: document.querySelector("#mainContent"),
+});
+
+window.addEventListener("hashchange", () => {
+  app.renderPage();
+});
+
+window.addEventListener("load", () => {
+  app.renderPage();
+});
