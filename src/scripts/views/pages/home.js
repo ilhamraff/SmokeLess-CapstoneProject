3fe6bf492962d81d8bc5719a235c02436bb19af4
ilhamraff <ifadhilah261@gmail.com>
2024-05-23 12:22:36 +0700

    membuat routes dan tampilan awal home

---
+++ src/scripts/views/pages/home.js
@@ -0,0 +1,24 @@
+const Home = {
+  async render() {
+    return `
+    <section class="content" id="welcome">
+        <article class="welcome-content">
+          <div class="content-description">
+            <h1 class="title">SELAMAT DATANG</h1>
+            <p>
+              Bersama Kami Berhenti Merokok<br />
+              dan Menuju Hidup yang Lebih Sehat
+            </p>
+            <button>Baca Selengkapnya</button>
+          </div>
+          <div class="content-image">
+            <img src="./src/public/images/1.png" alt="Dicoding Indonesia" />
+          </div>
+        </article>
+    </section>`;
+  },
+
+  async afterRender() {},
+};
+
+export default Home;
