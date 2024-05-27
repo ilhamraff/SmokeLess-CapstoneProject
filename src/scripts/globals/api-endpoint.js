5e62b6b499096a931f9bb9855606b142ea3732ab
ilhamraff <ifadhilah261@gmail.com>
2024-05-27 13:39:27 +0700

    menambahkan url api

---
+++ src/scripts/globals/api-endpoint.js
@@ -0,0 +1,7 @@
+import CONFIG from "./config";
+
+const API_ENDPOINT = {
+  GET_ARTICLE: `${CONFIG.BASE_URL}article.json`,
+};
+
+export default API_ENDPOINT;
---
+++ src/scripts/globals/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  BASE_URL: "https://deploy-test-99c00-default-rtdb.firebaseio.com/",
+  DEFAULT_LANGUAGE: "en-us",
+};
+
+export default CONFIG;
