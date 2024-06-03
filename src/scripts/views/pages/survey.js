e52f19669bf6082ac0cdaaa30d8ee8679db1be0a
Harfin <harfinaqbil07@gmail.com>
2024-06-03 16:53:41 +0700

    menambahkan function pada validasi form

--- src/scripts/views/pages/survey.js
+++ src/scripts/views/pages/survey.js
@@ -17,67 +17,67 @@ const Survey = {
             <h2>Formulir Perokok</h2>
             <form>
                 
-                <p>1. Usia:</p>
-                <input type="radio" name="age" value="under18" /> Di bawah 18 tahun<br />
-                <input type="radio" name="age" value="18-24" /> 18-24 tahun<br />
-                <input type="radio" name="age" value="25-34" /> 25-34 tahun<br />
-                <input type="radio" name="age" value="35-44" /> 35-44 tahun<br />
-                <input type="radio" name="age" value="45-54" /> 45-54 tahun<br />
-                <input type="radio" name="age" value="55-64" /> 55-64 tahun<br />
-                <input type="radio" name="age" value="65plus" /> 65 tahun ke atas<br /><br />
-                
-                <p>2. Jenis Kelamin:</p>
-                <input type="radio" name="gender" value="male" /> Laki-laki<br />
-                <input type="radio" name="gender" value="female" /> Perempuan<br />
-                <input type="radio" name="gender" value="other" /> Lainnya<br /><br />
-                
-                <p>3. Status Pendidikan:</p>
-                <input type="radio" name="education" value="none" /> Tidak/belum sekolah<br />
-                <input type="radio" name="education" value="elementary" /> SD/Sederajat<br />
-                <input type="radio" name="education" value="junior_high" /> SMP/Sederajat<br />
-                <input type="radio" name="education" value="senior_high" /> SMA/Sederajat<br />
-                <input type="radio" name="education" value="diploma" /> Diploma<br />
-                <input type="radio" name="education" value="bachelor" /> Sarjana (S1)<br />
-                <input type="radio" name="education" value="postgraduate" /> Pascasarjana (S2/S3)<br /><br />
-                
-                <p>4. Pekerjaan:</p>
-                <input type="radio" name="occupation" value="student" /> Pelajar/Mahasiswa<br />
-                <input type="radio" name="occupation" value="private_employee" /> Pegawai Swasta<br />
-                <input type="radio" name="occupation" value="civil_servant" /> Pegawai Negeri<br />
-                <input type="radio" name="occupation" value="entrepreneur" /> Wirausaha<br />
-                <input type="radio" name="occupation" value="freelancer" /> Pekerja Lepas/Freelancer<br />
-                <input type="radio" name="occupation" value="unemployed" /> Tidak bekerja<br />
-                <input type="radio" name="occupation" value="other" /> Lainnya<br /><br />
+              <p>1. Usia:</p>
+              <input type="radio" name="age" value="0" /> Di bawah 18 tahun<br />
+              <input type="radio" name="age" value="0" /> 18-24 tahun<br />
+              <input type="radio" name="age" value="0" /> 25-34 tahun<br />
+              <input type="radio" name="age" value="0" /> 35-44 tahun<br />
+              <input type="radio" name="age" value="0" /> 45-54 tahun<br />
+              <input type="radio" name="age" value="0" /> 55-64 tahun<br />
+              <input type="radio" name="age" value="0" /> 65 tahun ke atas<br /><br />
+              
+              <p>2. Jenis Kelamin:</p>
+              <input type="radio" name="gender" value="0" /> Laki-laki<br />
+              <input type="radio" name="gender" value="0" /> Perempuan<br />
+              <input type="radio" name="gender" value="0" /> Lainnya<br /><br />
+              
+              <p>3. Status Pendidikan:</p>
+              <input type="radio" name="education" value="0" /> Tidak/belum sekolah<br />
+              <input type="radio" name="education" value="0" /> SD/Sederajat<br />
+              <input type="radio" name="education" value="0" /> SMP/Sederajat<br />
+              <input type="radio" name="education" value="0" /> SMA/Sederajat<br />
+              <input type="radio" name="education" value="0" /> Diploma<br />
+              <input type="radio" name="education" value="0" /> Sarjana (S1)<br />
+              <input type="radio" name="education" value="0" /> Pascasarjana (S2/S3)<br /><br />
+              
+              <p>4. Pekerjaan:</p>
+              <input type="radio" name="occupation" value="0" /> Pelajar/Mahasiswa<br />
+              <input type="radio" name="occupation" value="0" /> Pegawai Swasta<br />
+              <input type="radio" name="occupation" value="0" /> Pegawai Negeri<br />
+              <input type="radio" name="occupation" value="0" /> Wirausaha<br />
+              <input type="radio" name="occupation" value="0" /> Pekerja Lepas/Freelancer<br />
+              <input type="radio" name="occupation" value="0" /> Tidak bekerja<br />
+              <input type="radio" name="occupation" value="0" /> Lainnya<br /><br />              
 
-                <p>5. Berapa banyak Anda merokok dalam sehari?</p>
-                <input type="radio" name="q1" value="0" /> < 10 batang/hari<br />
-                <input type="radio" name="q1" value="1" /> 11-20 batang/hari<br />
-                <input type="radio" name="q1" value="2" /> 21-30 batang/hari<br />
-                <input type="radio" name="q1" value="3" /> >30 batang/hari<br /><br />
+              <p>5. Berapa banyak Anda merokok dalam sehari?</p>
+              <input type="radio" name="q1" value="0" /> < 10 batang/hari<br />
+              <input type="radio" name="q1" value="1" /> 11-20 batang/hari<br />
+              <input type="radio" name="q1" value="2" /> 21-30 batang/hari<br />
+              <input type="radio" name="q1" value="3" /> >30 batang/hari<br /><br />
+          
+              <p>6. Seberapa cepat Anda merokok setelah bangun tidur?</p>
+              <input type="radio" name="q2" value="2" /> 5 menit setelah bangun tidur<br />
+              <input type="radio" name="q2" value="1" /> 6-30 menit setelah bangun tidur<br />
+              <input type="radio" name="q2" value="0" /> > 30 menit setelah bangun tidur<br /><br />
+          
+              <p>7. Apakah Anda merasa kesulitan untuk tidak merokok di “no smoking area”?</p>
+              <input type="radio" name="q3" value="1" /> Ya<br />
+              <input type="radio" name="q3" value="0" /> Tidak<br /><br />
+          
+              <p>8. Apakah Anda kesulitan untuk tidak merokok di pagi hari?</p>
+              <input type="radio" name="q4" value="1" /> Ya<br />
+              <input type="radio" name="q4" value="0" /> Tidak<br /><br />
+          
+              <p>9. Apakah Anda lebih sering merokok saat bekerja/belajar daripada saat jam istirahat?</p>
+              <input type="radio" name="q5" value="1" /> Ya<br />
+              <input type="radio" name="q5" value="0" /> Tidak<br /><br />
+          
+              <p>10. Apakah Anda masih merokok saat sakit?</p>
+              <input type="radio" name="q6" value="1" /> Ya<br />
+              <input type="radio" name="q6" value="0" /> Tidak<br /><br />
             
-                <p>6. Seberapa cepat Anda merokok setelah bangun tidur?</p>
-                <input type="radio" name="q2" value="2" /> 5 menit setelah bangun tidur<br />
-                <input type="radio" name="q2" value="1" /> 6-30 menit setelah bangun tidur<br />
-                <input type="radio" name="q2" value="0" /> > 30 menit setelah bangun tidur<br /><br />
-            
-                <p>7. Apakah Anda merasa kesulitan untuk tidak merokok di “no smoking area”?</p>
-                <input type="radio" name="q3" value="1" /> Ya<br />
-                <input type="radio" name="q3" value="0" /> Tidak<br /><br />
-            
-                <p>8. Apakah Anda kesulitan untuk tidak merokok di pagi hari?</p>
-                <input type="radio" name="q4" value="1" /> Ya<br />
-                <input type="radio" name="q4" value="0" /> Tidak<br /><br />
-            
-                <p>9. Apakah Anda lebih sering merokok saat bekerja/belajar daripada saat jam istirahat?</p>
-                <input type="radio" name="q5" value="1" /> Ya<br />
-                <input type="radio" name="q5" value="0" /> Tidak<br /><br />
-            
-                <p>10. Apakah Anda masih merokok saat sakit?</p>
-                <input type="radio" name="q6" value="1" /> Ya<br />
-                <input type="radio" name="q6" value="0" /> Tidak<br /><br />
-            
-                <button type="button" class="hitung">Hitung Kecanduan</button>
-                <button type="button" class="clear">Bersihkan Jawaban</button> 
+              <button type="button" class="hitung">Hitung Kecanduan</button>
+              <button type="button" class="clear">Bersihkan Jawaban</button> 
                 
             </form>
             <h2>Hasil Kecanduan:</h2>
@@ -136,44 +136,48 @@ const Survey = {
     },
 
     hitungKecanduan() {
-        var skor = 0;
+      var questions = ['age','gender','education','occupation', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6'];
+      var allAnswered = true;
+      var firstUnanswered = null;
     
-        // Mengambil nilai dari jawaban yang dipilih
-        skor += parseInt(
-          document.querySelector('input[name="q1"]:checked').value
-        );
-        skor += parseInt(
-          document.querySelector('input[name="q2"]:checked').value
-        );
-        skor += parseInt(
-          document.querySelector('input[name="q3"]:checked').value
-        );
-        skor += parseInt(
-          document.querySelector('input[name="q4"]:checked').value
-        );
-        skor += parseInt(
-          document.querySelector('input[name="q5"]:checked').value
-        );
-        skor += parseInt(
-          document.querySelector('input[name="q6"]:checked').value
-        );
+      questions.forEach(function(question) {
+          if (!document.querySelector('input[name="' + question + '"]:checked')) {
+              allAnswered = false;
+              if (!firstUnanswered) {
+                  firstUnanswered = question;
+              }
+          }
+      });
     
-        // Menentukan tingkat kecanduan berdasarkan skor
-        var tingkatKecanduan = "";
+      if (!allAnswered) {
+          alert('Harap isi semua pertanyaan.');
+          document.querySelector('input[name="' + firstUnanswered + '"]').scrollIntoView();
+          document.querySelector('input[name="' + firstUnanswered + '"]').focus();
+          return;
+      }
     
-        if (skor <= 2) {
+      var skor = 0;
+    
+      // Mengambil nilai dari jawaban yang dipilih
+      questions.forEach(function(question) {
+          skor += parseInt(document.querySelector('input[name="' + question + '"]:checked').value);
+      });
+    
+      // Menentukan tingkat kecanduan berdasarkan skor
+      var tingkatKecanduan = "";
+    
+      if (skor <= 2) {
           tingkatKecanduan = "Ketergantungan rendah";
-        } else if (skor <= 4) {
+      } else if (skor <= 4) {
           tingkatKecanduan = "Ketergantungan rendah sampai sedang";
-        } else if (skor <= 7) {
+      } else if (skor <= 7) {
           tingkatKecanduan = "Ketergantungan sedang";
-        } else {
+      } else {
           tingkatKecanduan = "Ketergantungan tinggi";
-        }
+      }
     
-        // Menampilkan hasil
-        document.getElementById("hasilKecanduan").innerHTML =
-          "Skor Anda: " + skor + " - " + tingkatKecanduan;
+      // Menampilkan hasil
+      document.getElementById("hasilKecanduan").innerHTML = "Skor Anda: " + skor + " - " + tingkatKecanduan;
     },
     
     clearForm() {
