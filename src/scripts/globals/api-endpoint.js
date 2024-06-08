2f13bec593d1d633edf590ac45ddb52afd6ffffc
ilhamraff <ifadhilah261@gmail.com>
2024-06-08 16:16:53 +0700

    memperbaiki style ukuran card list artikel

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,10 +1,10 @@
 import CONFIG from "./config";
 
 const API_ENDPOINT = {
-  ARTICLE: `${CONFIG.BASE_URL}article.json`,
-  COMMENTS: `${CONFIG.BASE_URL}comment.json`,
+  ARTICLE: `${CONFIG.BASE_URL}articles.json`,
+  COMMENTS: `${CONFIG.BASE_URL}comments.json`,
   DETAIL_ARTICLE: (id) =>
-    `https://smokeless-web-default-rtdb.firebaseio.com/article/${id}.json`,
+    `https://smokeless-web-default-rtdb.firebaseio.com/articles/${id}.json`,
 };
 
 export default API_ENDPOINT;
