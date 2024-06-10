3c1362b50216b144a6b4dac3eeeb0eec7d7147a9
ilhamraff <ifadhilah261@gmail.com>
2024-06-10 18:57:47 +0700

    membuat dokumentasi sederhana untuk globals

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,8 +1,26 @@
+/**
+ * API_ENDPOINT adalah objek yang berisi endpoint-endpoint API yang digunakan dalam aplikasi.
+ */
 import CONFIG from "./config";
 
 const API_ENDPOINT = {
+  /**
+   * Endpoint untuk mengambil daftar artikel.
+   * @type {string}
+   */
   ARTICLE: `${CONFIG.BASE_URL}articles.json`,
+
+  /**
+   * Endpoint untuk mengambil daftar komentar.
+   * @type {string}
+   */
   COMMENTS: `${CONFIG.BASE_URL}comments.json`,
+
+  /**
+   * Membuat endpoint untuk mengambil detail artikel berdasarkan ID.
+   * @param {string} id - ID artikel yang ingin diambil detailnya.
+   * @returns {string} Endpoint untuk mengambil detail artikel.
+   */
   DETAIL_ARTICLE: (id) => `${CONFIG.BASE_URL}articles/${id}.json`,
 };
 
--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,4 +1,11 @@
+/**
+ * CONFIG adalah objek yang berisi konfigurasi dasar untuk aplikasi, termasuk BASE_URL untuk mengakses API.
+ */
 const CONFIG = {
+  /**
+   * BASE_URL adalah URL dasar untuk mengakses API.
+   * @type {string}
+   */
   BASE_URL: "https://smokeless-web-default-rtdb.firebaseio.com/",
 };
 
