8a81002eca1929a18b11da67794ab770e52ff314
ilhamraff <ifadhilah261@gmail.com>
2024-06-15 00:03:59 +0700

    menambahkan fitur pencarian artikel

--- src/scripts/views/pages/article.js
+++ src/scripts/views/pages/article.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-inner-declarations */
 /* eslint-disable no-restricted-syntax */
 import ArticleSource from '../../data/article-source';
 import { createArticleCard } from '../templates/template';
@@ -15,6 +16,7 @@ const Article = {
             rekomendasikan pada halaman ini, selamat membaca.
           </p>
         </div>
+        <input type="text" id="searchInput" placeholder="Cari Artikel..." />
         <div class="go-to-add">
           <i class="fa fa-plus" aria-hidden="true"></i>
           <a href="#/add-article">Buat Artikel</a>
@@ -28,24 +30,41 @@ const Article = {
     try {
       const articles = await ArticleSource.getArticle(); // Ubah objek menjadi array
       const articlesContainer = document.querySelector('#articles');
+      const searchInput = document.querySelector('#searchInput');
 
-      for (const [id, article] of Object.entries(articles)) {
-        const card = document.createElement('div');
-        card.className = 'article-item';
-        card.dataset.id = id;
+      function renderArticles(filteredArticles) {
+        articlesContainer.innerHTML = ''; // Kosongkan kontainer artikel terlebih dahulu
+        for (const [id, article] of Object.entries(filteredArticles)) {
+          const card = document.createElement('div');
+          card.className = 'article-item';
+          card.dataset.id = id;
 
-        card.innerHTML = createArticleCard(article, id);
+          card.innerHTML = createArticleCard(article, id);
 
-        card.addEventListener('click', () => {
-          window.location.hash = `#/detail/${id}`;
-        });
+          card.addEventListener('click', () => {
+            window.location.hash = `#/detail/${id}`;
+          });
 
-        articlesContainer.appendChild(card);
+          articlesContainer.appendChild(card);
+        }
       }
+
+      // Render semua artikel saat pertama kali dimuat
+      renderArticles(articles);
+
+      // Tambahkan event listener untuk input pencarian
+      searchInput.addEventListener('input', (event) => {
+        const searchTerm = event.target.value.toLowerCase();
+        const filteredArticles = Object.entries(articles)
+          .filter(([id, article]) => article.title.toLowerCase().includes(searchTerm));
+
+        renderArticles(Object.fromEntries(filteredArticles));
+      });
     } catch (error) {
       console.error('Error rendering articles:', error);
     }
   },
+
 };
 
 export default Article;
